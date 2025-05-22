5d47925ac9c4b58533b64de5dadec7a54d08472a
Sahil Pahuja <19059570004@aryabhattacollege.ac.in>
2025-05-22 19:36:39 +0530

    basic query and upload routes working

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 QDRANT_URL = os.getenv("QDRANT_URL")
 QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
