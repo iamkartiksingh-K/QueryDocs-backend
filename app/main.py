a4a9d17e152cc7dd74d14bc1f81d42ebfb17939d
iamkartiksingh-K <kartiksinghk2001@gmail.com>
2025-05-24 16:01:09 +0530

    Authentication added

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer
+from app.database import Base, engine
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router
+from app.api.auth import router as authRouter
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
 app.include_router(router)
+app.include_router(authRouter, prefix="/auth", tags=["Auth"])
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 app.add_middleware(
     CORSMiddleware,
