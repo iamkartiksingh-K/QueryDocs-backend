c6374f5239d84af832553c65d86e7d7dab46511d
iamkartiksingh-K <kartiksinghk2001@gmail.com>
2025-05-28 01:57:40 +0530

    updated user schema, added profile route and verify access token function

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,12 +1,14 @@
 from pydantic import BaseModel, EmailStr
 
 class UserCreate(BaseModel):
+    name: str
     email: EmailStr
     password: str
 
 class UserOut(BaseModel):
     id: int
     email: EmailStr
+    name: str
     class Config:
         orm_mode = True
 
