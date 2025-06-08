c9b0f0174fa31faa42dbd553a35d4854c54ae8d3
Sahil Pahuja <19059570004@aryabhattacollege.ac.in>
2025-06-08 22:29:31 +0530

    api and url imported from config file now

--- app/vector_store/delete.py
+++ app/vector_store/delete.py
@@ -1,10 +1,12 @@
 from qdrant_client import QdrantClient
 from qdrant_client.http.models import Filter, FilterSelector
+from app.core.config import QDRANT_URL, QDRANT_API_KEY
+
 
 # Initialize client
 client = QdrantClient(
-    url="https://f69bf110-493b-4940-bf0a-ae80fbd5a357.europe-west3-0.gcp.cloud.qdrant.io",
-    api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0._aZaavXaH9k6DfQ8F8JXBp2OAUkQEh-pMCR-Y4rcVZ0"
+    url=QDRANT_URL,
+    api_key=QDRANT_API_KEY
 )
 
 # Use the correct points selector object
